f9f4130ec4f58e191e2e66b1bfc08cf57f223dea
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2025-04-20 17:43:21 +0500

    catalog api initial

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+// Cogure the http request pipeline
+app.MapCarter();
+
+app.Run();
