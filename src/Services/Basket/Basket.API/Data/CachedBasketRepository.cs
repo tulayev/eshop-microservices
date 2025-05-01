770c776f207e24f81979e1e1feaba5e14aa7dd73
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2025-05-01 22:18:16 +0500

    redis cache added to basket api

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository : IBasketRepository
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IDistributedCache _cache;
+
+        public CachedBasketRepository(
+            IBasketRepository basketRepository,
+            IDistributedCache cache)
+        {
+            _basketRepository = basketRepository;
+            _cache = cache;
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await _cache.GetStringAsync(userName, cancellationToken);
+
+            if (!string.IsNullOrWhiteSpace(cachedBasket)) 
+            {
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+            }
+
+            var basket = await _basketRepository.GetBasketAsync(userName, cancellationToken);
+            await _cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await _basketRepository.StoreBasketAsync(basket, cancellationToken);
+
+            await _cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
+
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            await _basketRepository.DeleteBasketAsync(userName, cancellationToken);
+
+            await _cache.RemoveAsync(userName, cancellationToken);
+
+            return true;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,27 +1,48 @@
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Behaviors.Logging;
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
