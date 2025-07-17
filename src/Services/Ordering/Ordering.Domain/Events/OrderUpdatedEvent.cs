d76aeb93995289fec333f319776d193ce37b6287
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2025-07-17 14:23:25 +0500

    feat: add domain layer for ordering service
    Introduces the initial domain model for the ordering service, including abstractions for entities and aggregates, domain events, exceptions, enums, models (Order, Customer, Product, OrderItem), and value objects (Address, Payment, various IDs). Also adds MediatR as a dependency for domain event handling.

---
+++ src/Services/Ordering/Ordering.Domain/Events/OrderUpdatedEvent.cs
@@ -0,0 +1,7 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Models;
+
+namespace Ordering.Domain.Events
+{
+    public record OrderUpdatedEvent(Order order) : IDomainEvent;
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,22 @@
+﻿using Ordering.Domain.Exceptions;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+        private OrderId(Guid value) => Value = value;
+
+        public static OrderId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (value == Guid.Empty)
+            {
+                throw new DomainException($"{nameof(OrderId)} cannot be empty.");
+            }
+
+            return new OrderId(value);
+        }
+    }
+}
