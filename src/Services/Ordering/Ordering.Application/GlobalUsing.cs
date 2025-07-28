04696825cf23ebaa51feeb5ff6b46fe1d12175bc
Umarbek Tulaev <69952016+tulayev@users.noreply.github.com>
2025-07-28 18:11:12 +0500

    feat: add CQRS, DTOs, and CRUD for Ordering Application
    Introduces CQRS command and query handlers, DTOs, validators, and event handlers for the Ordering application. Adds pagination support, exception handling, and global usings. Updates dependency injection to register behaviors and the application DB context. Modifies ApplicationDbContext to implement IApplicationDbContext and updates project references.

---
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Pagination;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
+global using Microsoft.EntityFrameworkCore;
