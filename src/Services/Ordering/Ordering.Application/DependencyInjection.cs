37d4d085d1e514c6944cae84721153c5e1ead368
Umarbek Tulayev <tumarbekttt@gmail.com>
2025-08-02 22:03:32 +0500

    feat: add feature flag for order fulfillment and RabbitMQ broker
    Introduced Microsoft.FeatureManagement for feature flags, added 'OrderFullfilment' flag to appsettings.json, and updated dependency injection to support feature management. Enhanced OrderCreatedEventHandler to publish integration events based on the feature flag. Added RabbitMQ message broker service to docker-compose files for messaging support.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using BuildingBlocks.Behaviors.Logging;
 using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 using System.Reflection;
 
 namespace Ordering.Application
@@ -18,6 +19,7 @@ namespace Ordering.Application
                 config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
 
+            services.AddFeatureManagement();
             services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
             return services;
